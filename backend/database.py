cf8a09ef8f430b0ef1fa69a0a545e88c27ecc878
nitz7155 <nitz7155@gmail.com>
2025-10-27 17:45:58 +0900

    Update Project

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from fastapi import Path
+from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.orm import Session
